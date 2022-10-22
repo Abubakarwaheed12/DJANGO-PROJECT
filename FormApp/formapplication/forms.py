75ad7cf282d999c691230dd7d588fa1359ddd809
abu <abubakarwaheed6346528@gmail.com>
2022-10-22 10:31:44 +0500

    orm API

--- FormApp/formapplication/forms.py
+++ FormApp/formapplication/forms.py
@@ -1,4 +1,20 @@
+import email
 from django import forms
 
-class myform(forms.Form):
-    label=a
+class user(forms.Form):
+    # FORM FIELD ARGUMENTS 
+    name=forms.CharField(label='enter name', label_suffix='', initial='jutt' , 
+    error_messages={'required':'name not enter'} ,)
+    lname=forms.CharField()
+    email=forms.EmailField()
+    # hiidden 
+    key=forms.CharField(widget=forms.HiddenInput())
+    
+    # BUILT IN WIDGETS examples
+    # DateInput
+    # textInput
+    
+    # PasswordInput
+    password=forms.CharField(widget=forms.TextInput(attrs={
+        'class':'form',
+    }))
--- FormApp/formapplication/views.py
+++ FormApp/formapplication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .forms import user 
 # Create your views here.
+
+def userdata(request):
+    #  THE AUTO ID IS USED TO GENERATE THE ID FOR INPUT TAG  
+    # AND THE LABEL_SUFFIX IS USED TO CONFIGURE THE LAbel
+    # AND INITIAL VALUE AT RUNTIME 
+    fm=user(auto_id='True' , label_suffix='......' , initial={'lname':'waheed','name':'abubakar', 'email':'AB@gmail.com', 'key':'snafkghdveshsb' , 'password':'acd'})
+    # order fields 
+    fm.order_fields(field_order=[ 'name', 'lname', 'email' ,])
+    return render(request, 'user.html' ,{'form':fm},)
