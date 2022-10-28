d115dc7a5ab19139eefdd73cd812fdf0285ded2a
abu <abubakarwaheed6346528@gmail.com>
2022-10-28 23:13:50 +0500

    --

--- dynamicurl/dynamic/views.py
+++ dynamicurl/dynamic/views.py
@@ -7,5 +7,6 @@ def home(request):
     return render(request, 'index.html')
 
 def student(request, my_id):
-    
+    if my_id==1:
+        name="abu bakar"
     return render(request , 'student.html' , {'id':my_id})
