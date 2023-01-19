302121971470ebc642255b2657dbc9e36518a756
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-01-19 17:14:42 +0500

    ..

--- cache/app/views.py
+++ cache/app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+# for per view cache 
+from django.views.decorators.cache import cache_page
+# we can use decorator function for making cache on view or we code on urls.py  
+# add middleware and caches in setting.py 
+
+# command for creating cache table 
+# python3 manage.py createcachetable  
+# File System Caching 
+# Local Caching 
+# On db caching 
+
+
+# we can also use template caching in our templates 
+
+# {% load cache %}
+# {% cache %}
 
 # Create your views here.
 
