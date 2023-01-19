c20aad760f0a627dfc02d9f2e2aceee237e3a553
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-01-19 14:02:50 +0500

    persite view

--- cache/cache/settings.py
+++ cache/cache/settings.py
@@ -31,12 +31,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'sitecache',
+    }
+}
+CACHE_MIDDLEWARE_SECONDS=20
 
 ROOT_URLCONF = 'cache.urls'
 
