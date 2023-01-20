48fe163c6b60e0cf2a85977575df2122a28612c2
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-01-20 11:55:27 +0500

    --

--- signals/app/signals.py
+++ signals/app/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.signals import user_logged_in , user_logged_out , user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import pre_init , pre_save , pre_delete , post_init , post_save , post_delete
+
 
 @receiver(user_logged_in , sender=User)
 def login_success(sender , request , user , **kwargs):
@@ -39,6 +41,13 @@ def log_out(sender , request , user , **kwargs):
     print('user : ' ,  user)
     print(f'kwargs  {kwargs}')
     
-    
-# user_logged_in.connect(login_success , sender=User)
-# we can connect with decorator example on above 
+
+
+# other model signal methods like pre init  , pre save etc 
+
+@receiver(pre_save, sender=User)
+def at_beg_savre(sender , instance , **kwargs):
+    print('The pre save function in run successsfully')
+    print('sender  :  ', sender)
+    print('Instance : ', instance)
+    print(f'kwargs :  {kwargs}')
