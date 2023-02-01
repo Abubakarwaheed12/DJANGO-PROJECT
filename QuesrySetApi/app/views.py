9a063aaf8ccf25b80044b59c7d41f4fa9303fe71
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-01 13:12:23 +0500

    ..

--- QuesrySetApi/app/views.py
+++ QuesrySetApi/app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from app.models import Student
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # std=Student.objects.all()    
+    # std=Student.objects.filter(marks=70)
+    # std=Student.objects.exclude(marks=70)
+    # for deascending order we will place a - tag in start of field name
+    # std=Student.objects.order_by('-name')
+    # std=Student.objects.order_by('id')
+    # std=Student.objects.reverse()[:2]
+    std=Student.objects.values()
+
+    print('SQL query' , std.query)
+    context={'students':std}
+    return render(request, 'index.html' , context=context)
