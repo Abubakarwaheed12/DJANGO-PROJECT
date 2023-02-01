0599d7105b41619aeeb47ac6a8afa838a263cf96
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-01 22:00:43 +0500

    all methods that return new query set or not

--- QuesrySetApi/app/views.py
+++ QuesrySetApi/app/views.py
@@ -3,6 +3,8 @@ from app.models import Student
 # Create your views here.
 
 def index(request):
+    #QUERY SET METHODS TAHT RETURN NEW QUERY SET  
+    
     # std=Student.objects.all()    
     # std=Student.objects.filter(marks=70)
     # std=Student.objects.exclude(marks=70)
@@ -10,8 +12,17 @@ def index(request):
     # std=Student.objects.order_by('-name')
     # std=Student.objects.order_by('id')
     # std=Student.objects.reverse()[:2]
-    std=Student.objects.values()
+    # std=Student.objects.values()
+    # std=Student.objects.values_list(flat=True)
 
+    # std=Student.objects.using('default')
+    # std=Student.objects.dates('pass_date', 'year')
+
+    std=Student.objects.all()
+    print(std)
     print('SQL query' , std.query)
     context={'students':std}
+    print(ts)
     return render(request, 'index.html' , context=context)
+
+# THERE ARE ALSO MANY METHODS TAHT NOT RETURN QUERY SET 
