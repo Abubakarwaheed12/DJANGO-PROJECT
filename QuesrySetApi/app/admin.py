52c9986ecd7765961f2b6c2e4a376dcfbcf6b365
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-01 11:33:03 +0500

    ..

--- QuesrySetApi/app/admin.py
+++ QuesrySetApi/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from app.models import Student
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['name', 'roll','city' , 'marks' , 'pass_date']
--- QuesrySetApi/app/models.py
+++ QuesrySetApi/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=70)
+    roll=models.IntegerField(unique=True, null=False)
+    city=models.CharField(max_length=70)
+    marks=models.IntegerField()
+    pass_date=models.DateTimeField()
+    
+    def __str__(self):
+        return self.name
