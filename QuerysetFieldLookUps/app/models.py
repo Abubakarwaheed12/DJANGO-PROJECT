fa7a8868b6ce552bfbf39d92841872f51ee24077
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-03 15:45:45 +0500

    .

--- QuerysetFieldLookUps/app/models.py
+++ QuerysetFieldLookUps/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=200)
+    roll=models.IntegerField(unique=True)
+    city=models.CharField(max_length=200)   
+    pass_date=models.DateField()
+    adm_date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
--- QuerysetFieldLookUps/app/views.py
+++ QuerysetFieldLookUps/app/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
-
+from app.models import Student
+from django.db.models import Avg ,Sum , Count ,Q , Max ,Min
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    # students=Student.objects.all()
+    # students=Student.objects.filter(roll__lt=3)
+    # students=Student.objects.filter(roll__lte=3)
+    # students=Student.objects.filter(roll__gt=3)
+    # students=Student.objects.filter(roll__gte=3)
+    # students=Student.objects.filter(roll__exact=3)
+    # students=Student.objects.filter(roll__startswith=3)
+    # students=Student.objects.filter(roll__gte=3)
+
+    # students=Student.objects.filter(name__contains ='abu')
+    # students=Student.objects.filter(name__icontains='Abu')
+
+    #AGGREGATION 
+    # students=Student.objects.filter(name__icontains='Abu')
+    # average=students.aggregate(Avg('roll'))
+    # print(average)
+    
+    # students=Student.objects.all()
+    # average=students.aggregate(Sum('roll'))
+    # print(average)
+    
+    # students=Student.objects.all()
+    # average=students.aggregate(Max('roll'))
+    # print(average)
+    
+    # students=Student.objects.all()
+    # average=students.aggregate(Min('roll'))
+    # print(average) 
+    
+    
+    
+    # Q object In DataBase 
+    students=Student.objects.filter(Q(roll=1) & Q(name='Abu Bakar Waheed'))
+    
+    
+    print(students.query)
+    context ={
+        'students':students,
+    }
+    return render(request, 'index.html',context)
