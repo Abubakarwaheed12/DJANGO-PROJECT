4fe3052601be08e3d4709b7266170232de9c35c7
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-14 12:12:49 +0500

    --

--- inheritenceModel/app/admin.py
+++ inheritenceModel/app/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from app.models import Student , Teacher
 # Register your models here.
-admin.site.register(Student)
+# admin.site.register(Student)
 admin.site.register(Teacher)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id' , 'name' , 'rollNo' , 'city']
+    
+    
+    
+    
+    
+    
+
+
+
+
+# Program 
+# l=[]
+# str='pynative'
+# for i in str:
+#     a=str.index(i)
+#     if a%2==0:
+#         l.append(str[a])
+# for val in l:
+#     print(f'{val}')
