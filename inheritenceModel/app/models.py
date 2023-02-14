da1d6d813b9dad524707e8760152bea1f33e266b
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-14 10:41:38 +0500

    ..

--- inheritenceModel/app/models.py
+++ inheritenceModel/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseClass(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    city=models.CharField(max_length=100)
+    
+    class Meta:
+        abstract=True
+        
+        
+class Student(BaseClass):
+    rollNo=models.IntegerField()
+    
+class Teacher(BaseClass):
+    grade=models.IntegerField()
