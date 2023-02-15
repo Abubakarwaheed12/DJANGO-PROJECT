293969f74808a81e7a4f7dd45f01b1b513294870
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-15 17:46:03 +0500

    ..

--- CrudClassBase/crud/urls.py
+++ CrudClassBase/crud/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from crud import views
 urlpatterns = [
-    path('' , views.home),
+    path('' , views.Home_View.as_view()),
+    # path('' , views.home),
     path('delete/<int:id>/' , views.delete , name='delete'),
     path('<int:id>' , views.update , name='update'),
 ]
--- CrudClassBase/crud/views.py
+++ CrudClassBase/crud/views.py
@@ -4,29 +4,37 @@ from unicodedata import name
 from django.shortcuts import render , HttpResponseRedirect
 from .forms import stureg
 from .models import user
+from django.views.generic.base import TemplateView
 # Create your views here.
 
-# view for home page 
 # Class Base View 
+class Home_View(TemplateView):
+    template_name='home.html'
 
+    def get_context_data(self  , *args , **kwargs):
+        context=super().get_context_data(**kwargs)
+        
+        fm=stureg
+        stud=user.objects.all()
+        return render (request , 'home.html', {'form':fm , 'stu':stud})
 
 
 # Function Base View 
-def home(request):
-    if request.method=='POST':
-        fm=stureg(request.POST)
-        if fm.is_valid():
-            nm=fm.cleaned_data['name']
-            em=fm.cleaned_data['email']
-            ps=fm.cleaned_data['password']
-            reg=user(name=nm , email=em , password=ps)
-            reg.save()
-            fm=stureg()
-        
-    else:
-        fm=stureg
-    stud=user.objects.all()
-    return render (request , 'home.html', {'form':fm , 'stu':stud})
+# def home(request):
+    # if request.method=='POST':
+        # fm=stureg(request.POST)
+        # if fm.is_valid():
+            # nm=fm.cleaned_data['name']
+            # em=fm.cleaned_data['email']
+            # ps=fm.cleaned_data['password']
+            # reg=user(name=nm , email=em , password=ps)
+            # reg.save()
+            # fm=stureg()
+        # 
+    # else:
+        # fm=stureg
+    # stud=user.objects.all()
+    # return render (request , 'home.html', {'form':fm , 'stu':stud})
 
 # for del
 def delete(request , id):
