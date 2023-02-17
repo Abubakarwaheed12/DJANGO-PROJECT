d31655eb568f7d3d5ddbaad94ef92002c89e3110
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-17 10:15:58 +0500

    ..

--- Ajax/core/app/views.py
+++ Ajax/core/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , HttpResponse
-
+from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
+    
     return render(request, 'index.html')
