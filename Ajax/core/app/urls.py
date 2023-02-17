053ae3c440c93f454db543c21460937217a0c635
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-17 14:30:05 +0500

    --

--- Ajax/core/app/urls.py
+++ Ajax/core/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from app.views import home
+from app.views import home , savename
 urlpatterns=[
     path('' , home , name='home'),
+    path('saved' , savename , name='saved'),
 ]
--- Ajax/core/app/views.py
+++ Ajax/core/app/views.py
@@ -5,3 +5,22 @@ from django.http import JsonResponse
 def home(request):
     
     return render(request, 'index.html')
+
+
+def savename(request):
+    if request.method=='POST':
+        nm=request.POST.get('name')
+        # a=len(nm)
+        count=0
+        for i in nm:
+            if i==' ':
+                count=count
+            else:
+                count+=1
+        
+        data={
+            'length':count,
+            'name':nm,
+        }
+        print(data)
+        return JsonResponse(data)    
