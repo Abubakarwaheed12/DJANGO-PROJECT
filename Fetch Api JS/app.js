f8da4cc4728e798b70cc5d3a3fcff0382dbe75e4
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-20 13:18:57 +0500

    axios

--- Fetch Api JS/app.js
+++ Fetch Api JS/app.js
@@ -1,11 +1,21 @@
-console.log('hello world')
+// console.log('hello world')
 
-const newReq=fetch(url , {
-    method:'POST',
-    headers:{
-        'accept':'application/json',
-        'content-type':'application/json',
-    },
-    body:body,
-    cache:'default',
-})
+// const newReq=fetch(url , {
+//     method:'POST',
+//     headers:{
+//         'accept':'application/json',
+//         'content-type':'application/json',
+//     },
+//     body:body,
+//     cache:'default',
+// })
+
+document.getElementById('btn').addEventListener('click' , makeRequest)
+
+function makeRequest(){
+    console.log('button clicked')
+    console.log('hello world')
+
+const newReq=fetch('data.txt')
+newReq.then((resolve)=>console.log(resolve))
+}
