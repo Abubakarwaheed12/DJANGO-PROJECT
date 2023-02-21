6ca42f29c70b50fb53b327de08ffd5ecbaef76f8
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-21 13:02:22 +0500

    -

--- FormView/app/models.py
+++ FormView/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactForm(models.Model):
+    name=models.CharField(max_length=200)
+    message=models.CharField(max_length=2000)
+    
+    
+    def __str__(self):
+        return f'{self.name} ({self.message}) '
