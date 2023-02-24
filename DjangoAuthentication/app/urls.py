8eea6de32649ebc3a9927d5207cb616d6ade712d
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-24 14:39:25 +0500

    -

--- DjangoAuthentication/app/urls.py
+++ DjangoAuthentication/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from app.views import profile
+from app.views import profile , clasbase_profile_view
+#Impor Template View   
+from django.views.generic.base import TemplateView
 urlpatterns=[
-    path('profile/' , profile , name='profile'),
+    # path('profile/' , profile , name='profile'),
+    path('p' , clasbase_profile_view.as_view(template_name='registration/profile.html')),
 ]
--- DjangoAuthentication/app/views.py
+++ DjangoAuthentication/app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render , HttpResponse
 from django.views import View 
 from django.views.generic.base import TemplateView
 from django.contrib.admin.views.decorators import staff_member_required   
-from django.utils.decorators import method_decorator
-# Create your views here.
+from django.utils.decorators import method_decorator 
+ 
+# Create your views here. 
 
 def profile(request):
     return render(request, 'registration/profile.html')
@@ -16,4 +17,4 @@ def profile(request):
 class clasbase_profile_view(TemplateView):
     template_name='registration/profile.html' 
     
-    
+#  Now Customization of Django Authentication
