e435bcac239205820468755030b704406e6ba7fb
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-24 10:15:15 +0500

    login required and other decorators in class based views

--- DjangoAuthentication/app/views.py
+++ DjangoAuthentication/app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render , HttpResponse
-
+from django.views import View 
+from django.views.generic.base import TemplateView
+from django.contrib.admin.views.decorators import staff_member_required   
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 def profile(request):
     return render(request, 'registration/profile.html')
+
+
+
+# Django Authentication with Class Based Views
+# And Authentication Way  
+@method_decorator(staff_member_required, name='dispatch')
+class clasbase_profile_view(TemplateView):
+    template_name='registration/profile.html' 
+    
+    
