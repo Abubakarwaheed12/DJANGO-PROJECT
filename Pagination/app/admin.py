f0fb9d7d09468b1e545bc5bd509bc6f4969ed971
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-27 13:15:01 +0500

    sdf

--- Pagination/app/admin.py
+++ Pagination/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from app.models import post 
 # Register your models here.
+@admin.register(post)
+class postAdmin(admin.ModelAdmin):
+    list_display=['id','name' , 'title']
--- Pagination/app/models.py
+++ Pagination/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+    name=models.CharField(max_length=190)
+    title=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name 
