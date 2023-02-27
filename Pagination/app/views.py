d632cecab02865c461394cad8b11f16c579a7f17
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-27 13:32:53 +0500

    ad

--- Pagination/app/views.py
+++ Pagination/app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from app.models import post
+from django.core.paginator import Paginator 
 # Create your views here.
 
 def home(request):
-    return render(request , 'index.html')
+    posts=post.objects.all()
+    paginator=Paginator(posts, 2)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
+    context={
+        'posts':posts,
+    }
+    return render(request , 'index.html' , context)
